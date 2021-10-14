f1e25c0f083806b170dce78c122a9faeb7d8e778
stemberk <stemberk@spsmb.cz>
2021-10-14 15:11:31 +0200

    zadani pravouhleho t.

---
+++ src/cz/spsmb/b3i/w07/excercise02/PravouhlyTrojuhelnik.java
@@ -0,0 +1,10 @@
+package cz.spsmb.b3i.w07.excercise02;
+
+//vytvořte třídu PravouhlyTrojuhelnik, kdy se jako parametry konstruktoru budou
+//zadávat pouze odvěsny a přepona se dopočítá. Třída bude poskytovat metodu
+//double delkaPrepony()
+
+//Přetěžte konstruktor PravouhlyTrojuhelnik, který vytvoří rovnoramenný trojúhelník
+//s odvěsnami délky1. Třída bude poskytovat metodu double obsah()
+public class PravouhlyTrojuhelnik {
+}
