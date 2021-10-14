8033723c5fa709b4ab20a7feb6e15f015b60194e
ondra <ondrajanosik283@gmail.com>
2021-10-14 16:58:21 +0200

    DOBREJ

--- src/cz/spsmb/b3i/w07/excercise02/PravouhlyTrojuhelnik.java
+++ src/cz/spsmb/b3i/w07/excercise02/PravouhlyTrojuhelnik.java
@@ -7,4 +7,25 @@ package cz.spsmb.b3i.w07.excercise02;
 //Přetěžte konstruktor PravouhlyTrojuhelnik, který vytvoří rovnoramenný trojúhelník
 //s odvěsnami délky1. Třída bude poskytovat metodu double obsah()
 public class PravouhlyTrojuhelnik {
+    private double stranaA;
+    private double stranaB;
+
+    public PravouhlyTrojuhelnik(double odvesnaA, double odvesnaB) {
+        this.stranaA = odvesnaA;
+        this.stranaB = odvesnaB;
+    }
+    public double delkaPrepony() {
+        return Math.sqrt((stranaA * stranaA) + (stranaB * stranaB));
+    }
+
+    public PravouhlyTrojuhelnik(double odvesnaA) {
+        this(odvesnaA,odvesnaA);
+    }
+    public double obsah() {
+        return (stranaA * stranaA)/2;
+    }
+
 }
+
+
+
