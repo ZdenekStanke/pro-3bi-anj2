a3e41df99738c67d997220ae9ae5f812ae0e2eb9
stemberk <stemberk@spsmb.cz>
2021-10-21 12:47:41 +0200

    DU+

---
+++ src/cz/spsmb/b3i/w08/DomaciUkol.java
@@ -0,0 +1,50 @@
+package cz.spsmb.b3i.w08;
+
+public class DomaciUkol {
+    /***
+     * Každý sem napište metodu třídy dle zadání
+     Černý
+     public static int obsahObdelnika(int stranaA, int stranaB)
+
+     Haken
+     public static int obsahPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
+
+     Chomanič
+     public static int obvodObdelnika(int stranaA, int stranaB)
+
+     Janošík
+     může si vybrat svou úlohu
+
+
+     Karban
+     public static double obsahKruhu(double polomer)
+
+     Kraušner
+     public static double obvodKruhu(double polomer)
+
+     Krouský
+     public static int povrchKrychle(int hranaA)
+
+     Matoušek
+     public static int objemKrychle(int hranaA)
+
+     Merhaut
+     public static int obvodPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
+
+     Skalický
+     public static long faktorial(byte n)
+
+     Stanke
+     Výpočet fibbonaciho posloupnosti
+     private static int fibonacci(int n)
+
+     Šípek
+     public static double povrchKoule(double polomer)
+
+     Zajíček
+     public static double objemKoule(double polomer)
+
+     Žárský
+     public static long mocnina(int cislo, byte mocnitel)
+     ***/
+}
