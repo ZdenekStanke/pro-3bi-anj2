c14aef8c1e9d02d2f3544540be3e43322355cf69
ondrej.zarsky <ondrej.zarsky@student.spsmb.cz>
2021-11-02 10:35:09 +0100

    úkol - Žárský

---
+++ zgzghjghjg/src/cz/spsmb/b3i/Main.java
@@ -0,0 +1,12 @@
+package cz.spsmb.b3i;
+
+public class Main {
+
+	public static long mocnina(int cislo, byte mocnitel){
+
+            int a;
+            a= (int) Math.pow(cislo, mocnitel);
+            return a;
+    }
+    }
+
