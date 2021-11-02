7293b869e31a39eb6e95830482c2afe541708e95
gitofson <stemberk@gmail.com>
2021-11-02 11:36:47 +0100

    Revert "úkol-Žárský"

--- src/cz/spsmb/b3i/w08/Cviceni/StringToBinary.java
+++ src/cz/spsmb/b3i/w08/Cviceni/StringToBinary.java
@@ -1,18 +1,8 @@
 package cz.spsmb.b3i.w08.Cviceni;
-
-import java.util.Scanner;
-
 //Napište program, který načítá řetězce složené z číslic. Pokud bude
 //první číslicí 0, je zbytek řetězce považovaný za osmičkové číslo.
 //Jsou-li na začátku znaky 0x (nebo 0X), pak se jedná o šestnáctkové
 // číslo. Všechna ostatní čísla jsou desítková čísla. Toto načtené číslo
 // vypište binárně
-//public class StringToBinary {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int a = sc.nextInt();
-//        if (a==0){
-//            System.out.format()
-//        }
-//    }
-//}
+public class StringToBinary {
+}
