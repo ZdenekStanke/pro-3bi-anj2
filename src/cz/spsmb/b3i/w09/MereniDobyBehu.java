8d81ff3293eeafa1e74b36ba67320b84d883091b
stemberk <stemberk@spsmb.cz>
2021-11-03 10:26:33 +0100

    doba behu +

--- src/cz/spsmb/b3i/w09/MereniDobyBehu.java
+++ src/cz/spsmb/b3i/w09/MereniDobyBehu.java
@@ -5,10 +5,12 @@ public class MereniDobyBehu {
         //currentTimeMillis vrací počet milisekund od 1.1.1970
         //význam při hledání doby běhu programu
         long t1 = System.currentTimeMillis();
-        cz.spsmb.b3i.w08.DomaciUkol.faktorial((byte)20);
+        long f1 = cz.spsmb.b3i.w08.DomaciUkol.faktorial((byte)19);
         long t2 = System.currentTimeMillis();
-        cz.spsmb.b3i.w08.DomaciUkol.faktorialRekurzivne((byte)20);
+        long f2 = cz.spsmb.b3i.w08.DomaciUkol.faktorialRekurzivne((byte)19);
         long t3 = System.currentTimeMillis();
+        System.out.format("20!=%d (%dms), rekurzivně:20!=%d (%dms)",
+                f1, t2-t1, f2, t3-t1);
     }
 
 }
