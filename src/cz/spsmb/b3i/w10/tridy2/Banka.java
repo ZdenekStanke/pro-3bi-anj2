5830f3bc4aa899b9f1b7306c8d78299a8d859211
stemberk <stemberk@spsmb.cz>
2021-11-10 11:23:01 +0100

    tridy2+

---
+++ src/cz/spsmb/b3i/w10/tridy2/Banka.java
@@ -0,0 +1,8 @@
+package cz.spsmb.b3i.w10.tridy2;
+
+//Vytvořte třídu bankovní účet s potřebnými datovými prvky a metodami. Ve třídě Banka
+//vytvořte pole účtů, které náhodně inicializujete. Vypište celkovou hotovost v bance.
+
+public class Banka {
+
+}
