348daac90528c173aba6a6a42b879f15682180da
stemberk <stemberk@spsmb.cz>
2021-11-11 14:53:57 +0100

    klient+

--- src/cz/spsmb/b3i/w10/tridy2/Klient.java
+++ src/cz/spsmb/b3i/w10/tridy2/Klient.java
@@ -1,5 +1,6 @@
 package cz.spsmb.b3i.w10.tridy2;
 
+import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
 //Vytvořte třídu Klient, která bude mít jako jednu členskou
@@ -10,6 +11,23 @@ public class Klient {
     private String jmeno;
     private String prijmeni;
     private String adresa;
-    GregorianCalendar datumNarozeni;
+    private GregorianCalendar datumNarozeni;
 
+    public Klient(String jmeno, String prijmeni, String adresa, GregorianCalendar datumNarozeni) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.adresa = adresa;
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    @Override
+    public String toString() {
+        return "Klient{" +
+                "jmeno='" + this.jmeno + '\'' +
+                ", prijmeni='" + this.prijmeni + '\'' +
+                ", adresa='" + this.adresa + '\'' +
+                ", datumNarozeni=" +
+                new SimpleDateFormat("dd.MM.YYYY hh:mm:ss a").format(this.datumNarozeni.getTime()) +
+                '}';
+    }
 }
