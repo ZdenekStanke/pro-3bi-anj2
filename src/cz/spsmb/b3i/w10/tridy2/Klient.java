fda1f71567c0601305a48f6d53f37b75d069619a
stemberk <stemberk@spsmb.cz>
2021-11-11 14:50:57 +0100

    klient+

--- src/cz/spsmb/b3i/w10/tridy2/Klient.java
+++ src/cz/spsmb/b3i/w10/tridy2/Klient.java
@@ -1,7 +1,15 @@
 package cz.spsmb.b3i.w10.tridy2;
+
+import java.util.GregorianCalendar;
+
 //Vytvořte třídu Klient, která bude mít jako jednu členskou
 // proměnnou referenci na objekt třídy BankovniUcet. Modifikujte třídu Banka,
 // aby pracovala s klienty
 // místo účty.
 public class Klient {
+    private String jmeno;
+    private String prijmeni;
+    private String adresa;
+    GregorianCalendar datumNarozeni;
+
 }
