a22022320d8867356b47f1220f28dfd1a884a199
stemberk <stemberk@spsmb.cz>
2021-11-18 13:21:03 +0100

    D+

---
+++ src/cz/spsmb/b3i/w12/dedicnost/Uvod.java
@@ -0,0 +1,7 @@
+package cz.spsmb.b3i.w12.dedicnost;
+//Dědičnost - možnost přidat k bázové třídě,
+// základní třídě, rodičovské třídě, supertřídě, rodiči, předkovi
+// další vlastnosti a odvodit tak další (zděděnou) třídu, resp.
+// potomka, resp. dceřinnou třídu.
+public class Uvod {
+}
