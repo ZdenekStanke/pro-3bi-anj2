3fca276114cc3de1ed16d99ebcbfefc2f2b1645b
stemberk <stemberk@spsmb.cz>
2021-11-18 15:11:44 +0100

    JednostopeVoyidlo +

---
+++ src/cz/spsmb/b3i/w12/cviceni_vozidlo/JednostopeVozidlo.java
@@ -0,0 +1,13 @@
+package cz.spsmb.b3i.w12.cviceni_vozidlo;
+
+//Vytvořte abstraktní třídu JednostopeVozidlo s proměnnou String druh
+// a abstraktní metodu void vypisCoJsiZac(), která bude v odvozených třídách
+// vypisovat obsah řetězce druh a další informace, např výrobce, cenu
+// atd. Od této třídy odvoďte třídu Kolo a třídu Motocykl (např.
+// s proměnnou obsahNadrze).
+
+//Ve třídě HorskeKolo překryjte metodu void vypisCoJsiZac() tak, aby
+// vypisovala i počty převodů.
+
+public abstract class JednostopeVozidlo {
+}
