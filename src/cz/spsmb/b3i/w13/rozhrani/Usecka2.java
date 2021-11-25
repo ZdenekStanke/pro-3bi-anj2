eff71d19bfd051cf9d5880e91423d72577d0133f
stemberk <stemberk@spsmb.cz>
2021-11-25 14:35:26 +0100

    Instance rozhraní může využívat jen metody rozhraní

--- src/cz/spsmb/b3i/w13/rozhrani/Usecka2.java
+++ src/cz/spsmb/b3i/w13/rozhrani/Usecka2.java
@@ -17,8 +17,19 @@ public class Usecka2 implements Info, InfoDalsi {
         System.out.println(" = " + this.delka);
     }
 
+    public int getDelka() {
+        return delka;
+    }
+
     public static void main(String[] args) {
         Usecka2 u = new Usecka2(5);
         u.kdoJsem(); u.vlastnosti();
+        Info info = new Usecka2(2);
+        info.kdoJsem();
+        //Instance rozhraní může využívat jen metody rozhraní:
+        //info.vlastnosti(); // toto nelze
+        ((Usecka2) info).vlastnosti();// po přetypování ano
+        //System.out.println(info.getDelka());//také nelze
+        System.out.println(((Usecka2)info).getDelka());//to už lze
     }
 }
