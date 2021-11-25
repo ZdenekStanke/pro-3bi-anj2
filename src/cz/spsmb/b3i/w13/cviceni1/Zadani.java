0a4c1fe6a0ed6346abc8b89e466fc2a9eba7ce1e
stemberk <stemberk@spsmb.cz>
2021-11-25 15:11:24 +0100

    zadani1

---
+++ src/cz/spsmb/b3i/w13/cviceni1/Zadani.java
@@ -0,0 +1,9 @@
+package cz.spsmb.b3i.w13.cviceni1;
+//Vytvořte třídy Ctverec a Usecka s vhodně zvolenými metodami a proměnnými.
+//Vytvořte rozhraní Zobrazitelný s metodou void zobraz(). Zajistěte, aby třídy
+//Ctverec a Usecka implementovaly toto rozhraní tak, že se vykreslí na obrazovku např.:
+//      ***
+//      ***             *************
+//      ***
+public class Zadani {
+}
