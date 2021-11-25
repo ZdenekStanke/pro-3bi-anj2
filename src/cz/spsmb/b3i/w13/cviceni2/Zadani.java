4a1886fce98c9e253f175f31a443f27b75c5b3c1
stemberk <stemberk@spsmb.cz>
2021-11-25 15:12:08 +0100

    zadani2

---
+++ src/cz/spsmb/b3i/w13/cviceni2/Zadani.java
@@ -0,0 +1,8 @@
+package cz.spsmb.b3i.w13.cviceni2;
+//Od třídy Ctverec odvoďte třídu DutyCtverec a zajistěte správnou implementaci
+//metody void zobraz(), např:
+//      ***
+//      * *
+//      ***
+public class Zadani {
+}
