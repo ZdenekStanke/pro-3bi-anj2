3bddd3f6b072a21bb42adb8f7d1c6c092efc76bf
stemberk <stemberk@spsmb.cz>
2021-12-01 11:25:25 +0100

    DoubleLinkedList a generika

--- src/cz/spsmb/b3i/w13/genericky_spojovy_seznam/DoubleLinkedList.java
+++ src/cz/spsmb/b3i/w13/genericky_spojovy_seznam/DoubleLinkedList.java
@@ -1,5 +1,7 @@
 package cz.spsmb.b3i.w13.genericky_spojovy_seznam;
 
+import java.util.Iterator;
+
 public class DoubleLinkedList implements Iterable<Integer> {
     Node head;
 
@@ -26,5 +28,26 @@ public class DoubleLinkedList implements Iterable<Integer> {
             n = n.next;
             i++;
         }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            Node currentNode = head;
+            @Override
+            public boolean hasNext() {
+                return this.currentNode != null;
+            }
+
+            @Override
+            public Integer next() {
+                if(this.hasNext()){
+                    int data = this.currentNode.data;
+                    this.currentNode = this.currentNode.next;
+                    return data;
+                }
+                return null;
+            }
+        };
     }
 }
