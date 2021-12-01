1cbbf7bed2fe63f14924128d9a0a35f1124fc9f2
stemberk <stemberk@spsmb.cz>
2021-12-01 11:08:48 +0100

    quick sort + bubble sort

--- src/cz/spsmb/b3i/w13/spojovy_seznam/Node.java
+++ src/cz/spsmb/b3i/w13/spojovy_seznam/Node.java
@@ -1,16 +1,24 @@
 package cz.spsmb.b3i.w13.spojovy_seznam;
 
-public class Node {
+public class Node implements Comparable {
     public int getData() {
         return data;
     }
 
-    private int data;
+    int data;
     Node next;
 
     public Node(int d)
     {
         this.data = d;
         this.next = null;
+    }
+
+    //vrací záporné číslo, pokud je argument větší,
+    //kladné, pokud je menší
+    //0, pokud je stejné
+    @Override
+    public int compareTo(Object o) {
+        return this.data - ((Node)o).data;
     }
 }
--- src/cz/spsmb/b3i/w13/spojovy_seznam/Test.java
+++ src/cz/spsmb/b3i/w13/spojovy_seznam/Test.java
@@ -1,23 +1,37 @@
 package cz.spsmb.b3i.w13.spojovy_seznam;
 
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args) {
+        Random r = new Random();
         LinkedList ll = new LinkedList();
         ll.append(123);
         ll.append(13);
         ll.append(1);
+        for (int i = 0; i < 150; i++) {
+            ll.append(r.nextInt(200));
+        }
+        /*
         ll.printList();
         Node n = ll.getHead();
         System.out.println();
         while (n!=null){
             System.out.println(n.getData()+' ');
             n=n.next;
-        }
+        }*/
         //Aby fungoval foreach typ cyklu, je potřeba implementovat rozhraní Iterable
-        for (Node n1:ll
+        int i=0;
+        for (Object n1:ll
              ) {
-            System.out.println(n.getData());
+            System.out.print(((i%10==0)?"\n":" ") + n1);
+            i++;
         }
+        Node n = ll.getHead();
+        while (n.next != null)
+            n = n.next;
+        ll.quickSort(ll.getHead(), n);
+        ll.printList();
     }
 
 
