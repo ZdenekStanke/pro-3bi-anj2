264b74d413874219d8d4a2c95ec123937256ac55
stemberk <stemberk@spsmb.cz>
2021-12-01 10:36:30 +0100

    test+

---
+++ src/cz/spsmb/b3i/w13/spojovy_seznam/Test.java
@@ -0,0 +1,20 @@
+package cz.spsmb.b3i.w13.spojovy_seznam;
+
+public class Test {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.append(123);
+        ll.append(13);
+        ll.append(1);
+        ll.printList();
+        Node n = ll.getHead();
+        System.out.println();
+        while (n!=null){
+            System.out.println(n.getData()+' ');
+            n=n.next;
+        }
+    }
+
+
+
+}
