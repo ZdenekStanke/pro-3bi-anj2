a988b631f2cc26256fe04c5f1590c72025de685e
stemberk <stemberk@spsmb.cz>
2021-12-02 15:09:13 +0100

    kolekce+

---
+++ src/cz/spsmb/b3i/w14/kolekce/Ukol.java
@@ -0,0 +1,6 @@
+package cz.spsmb.b3i.w14.kolekce;
+//ArrayList lze vytvořit i s předem vytvořeným počtem prvků. Takto:
+//ar = new ArrayList<Integer>(N). Proveďte rychlostní test a srovnejte
+//s ar = new ArrayList<Integer>() pro milión prvků;
+public class Ukol {
+}
