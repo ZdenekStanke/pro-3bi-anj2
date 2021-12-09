74ff12e21fbec65d7df4d3826460cfb93ccc21de
stemberk <stemberk@spsmb.cz>
2021-12-09 15:17:13 +0100

    ukol

---
+++ src/cz/spsmb/b3i/w15/trida_linked_list/ukol_fronta/Fronta.java
@@ -0,0 +1,8 @@
+package cz.spsmb.b3i.w15.trida_linked_list.ukol_fronta;
+//Domácí úkol do 16.12.:
+//použijte LinkedList pro implementaci abstraktního datového typu fronta
+//tj. totéž, co bylo uděláno v třídách Zasobnik a PouzitiZasobniku analogicky
+//pro frontu
+public class Fronta {
+
+}
