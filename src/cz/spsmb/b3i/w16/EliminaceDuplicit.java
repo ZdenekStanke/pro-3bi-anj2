34ed8c1332717bf115ab4f99f4125c762ff24f9d
stemberk <stemberk@spsmb.cz>
2021-12-16 14:56:29 +0100

    eliminace duplicit - trik

--- src/cz/spsmb/b3i/w16/EliminaceDuplicit.java
+++ src/cz/spsmb/b3i/w16/EliminaceDuplicit.java
@@ -2,6 +2,7 @@ package cz.spsmb.b3i.w16;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class EliminaceDuplicit {
     public static void main(String[] args) {
@@ -10,5 +11,7 @@ public class EliminaceDuplicit {
         d.add("druhy");
         d.add("prvni");
         System.out.println("Duplicitni: " + d);
+        Collection<String> nd = new ArrayList<>(new HashSet<>(d));
+        System.out.println("NeDuplicitni: " + nd);
     }
 }
