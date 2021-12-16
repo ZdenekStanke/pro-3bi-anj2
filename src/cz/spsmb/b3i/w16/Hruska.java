140e498599b6ff00b9f10c8d211458420f2a3092
stemberk <stemberk@spsmb.cz>
2021-12-16 14:36:20 +0100

    Vlastní třída v množině metody tridy Object equals() a hashCode()

--- src/cz/spsmb/b3i/w16/Hruska.java
+++ src/cz/spsmb/b3i/w16/Hruska.java
@@ -1,6 +1,6 @@
 package cz.spsmb.b3i.w16;
 
-public class Hruska {
+public class Hruska implements Comparable<Hruska>{
     private int cena;
 
     public Hruska(int cena) {
@@ -15,5 +15,31 @@ public class Hruska {
     }
     public void tisk() {
         System.out.println(this.cena + ", ");
+    }
+
+    @Override
+    public int compareTo(Hruska o) {
+        if(this.cena > o.cena) {
+            return +1;
+        } else if (this.cena == o.cena){
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+    @Override
+    public int hashCode() {
+        return this.cena;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        if(obj instanceof Hruska == false) {
+            return false;
+        }
+        return (cena == ((Hruska) obj).cena);
     }
 }
