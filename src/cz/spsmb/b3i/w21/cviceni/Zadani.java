5944892b00cde1681e6ae8ab7d2b9763bc6f4918
stemberk <stemberk@spsmb.cz>
2022-01-25 14:46:21 +0100

    cviceni zadani

---
+++ src/cz/spsmb/b3i/w21/cviceni/Zadani.java
@@ -0,0 +1,11 @@
+package cz.spsmb.b3i.w21.cviceni;
+
+// 1. Napište program, který vypisuje jména všech podadresářů v zadaném rodičovském adresáři.
+// 2. Modifikujte předchozí program tak, že budou vypisovány i všechny vnořené adresáře
+//    z těchto podadresářů.
+// 3. Napište program, kreý vypíše podle zadané masky všechny soubory, které se nacházejí v zadaném
+//    rodičovském adresáři.
+// 4. Modifikujte předchozí program tak, že budou vypisovány i soubory ze všech vnořených adresářů
+//    z těchto podadresářů.
+public class Zadani {
+}
