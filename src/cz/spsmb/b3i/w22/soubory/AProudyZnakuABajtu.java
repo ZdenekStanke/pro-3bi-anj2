245255e7d4bed6313f43258e0b74dd81d5fd702f
stemberk <stemberk@spsmb.cz>
2022-01-31 11:50:03 +0100

    w22

--- src/cz/spsmb/b3i/w22/soubory/AProudyZnakuABajtu.java
+++ src/cz/spsmb/b3i/w22/soubory/AProudyZnakuABajtu.java
@@ -4,7 +4,24 @@ package cz.spsmb.b3i.w22.soubory;
 // výstupní proud - zápis
 // Java rozlišuje proudy znaků, kde základní jednotka je 16bitová pro Unicode znaky
 // a proub bytů, kte je zákl. jednotka 8bitová.
+
 // Znakově orientované proudy:
-// Základní abstraktní třídy Reader, Writer
+// základní abstraktní třídy Reader, Writer
+
+// Bytově orientované proudy:
+// základní abstraktní třídy InputStream a OutputStream
+
+// Od výše uvedených tříd se dědí další, které se již běžně používají.
+// Všechny poskytují druh 3x přetížené metody, kterou si pracovně nazvema jako rw():
+// rw() - práce s jedním prvkem
+// rw(typ[] pole) - práce s celým polem prvků
+// rw(typ[] pole, int index, int pocet) - práce s částí pole
+// týká se to metdo read() a write() u všech tříd. Tyto metody vrací hodnotu -1,
+// pokud bylo dosaženo konce proudu (tj. už není co číst).
+// Třída Writer obsahuje navíc ještě tyto 2 metody:
+// void write(String retez)
+// void write(String retez, int index, int pocet)
+
+
 public class AProudyZnakuABajtu {
 }
