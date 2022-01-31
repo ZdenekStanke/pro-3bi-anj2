12f6f5c06b8b7f95985204b34d00c63a86625f2a
stemberk <stemberk@spsmb.cz>
2022-01-31 11:20:31 +0100

    Zadani

---
+++ src/cz/spsmb/b3i/w22/soubory/AProudyZnakuABajtu.java
@@ -0,0 +1,10 @@
+package cz.spsmb.b3i.w22.soubory;
+//Pro jakýkoliv přenos informace je nutné otevřít tzv. prud (stream).
+// vstupní proud - čtení
+// výstupní proud - zápis
+// Java rozlišuje proudy znaků, kde základní jednotka je 16bitová pro Unicode znaky
+// a proub bytů, kte je zákl. jednotka 8bitová.
+// Znakově orientované proudy:
+// Základní abstraktní třídy Reader, Writer
+public class AProudyZnakuABajtu {
+}
--- src/cz/spsmb/b3i/w22/soubory/ukol/Zadani.java
+++ src/cz/spsmb/b3i/w22/soubory/ukol/Zadani.java
@@ -1,8 +1,4 @@
 package cz.spsmb.b3i.w22.soubory.ukol;
-
-//1, Napište program, který vypisuje jména všech podadresářů v zadaném rodičovském adresíři.
-//2, Modifikujte předchozí program tak, že budou vypisovány i všechny vnořené adresáře
-// z těchto podadresářů
 //3, Napište program, který vypíše podle zadané masky všechny soubory, keré se nacházejí
 // v zadaném rodičovském adresáři.
 //4, Modifikujte předchozí program tak, že budou vypisovány i soubory ze všech vnořených adresářů
