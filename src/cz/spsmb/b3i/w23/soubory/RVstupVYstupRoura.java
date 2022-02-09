0e3f902c1e4412d779e64b9bbc28050cb68ec939
stemberk <stemberk@spsmb.cz>
2022-02-09 11:08:48 +0100

    RVstupVYstupRoura

--- src/cz/spsmb/b3i/w23/soubory/RVstupVYstupRoura.java
+++ src/cz/spsmb/b3i/w23/soubory/RVstupVYstupRoura.java
@@ -10,11 +10,17 @@ import java.io.*;
 // která by se jinakukládala nejčastěji do pomocného souboru a ten
 // by se pos končení programu vymazal. Roura tím, že je vzniká
 // v paměti je však mnohem rychlejší.
+
+//Příklad: Čtěte soubor, který obsahuje na každé řádce jedno celé číslo. Vypište
+// z něj čísla, která mají jako první znak 1. Z těchto čísel pak dalším čtením
+// vyberte pouze ta, jejichž třetí znak je 9.
 public class RVstupVYstupRoura {
+    //Návratový typ bude roura
     public static Reader vyber(Reader vstup, int index, char znak) throws IOException {
         BufferedReader bufVstup = new BufferedReader(vstup);
         //Výstup do roury
         PipedWriter rouraVystup = new PipedWriter();
+
         PipedReader rouraVstup = new PipedReader(rouraVystup);
         PrintWriter formRouraVystup = new PrintWriter(rouraVystup);
         String radka;
@@ -28,7 +34,15 @@ public class RVstupVYstupRoura {
     }
 
     public static void main(String[] args) throws IOException {
+        FileWriter fw =new FileWriter("data120.txt");
+        for (int i = 0; i <= 120; i++) {
+            fw.write(String.format("%d%n",i));
+
+        }
+        fw.close();
         FileReader fr = new FileReader("data120.txt");
+        //1. hledám znak '1' na pozici 0
+        //2. hledám znak '9' na pozici 2
         Reader jednaDevet = vyber(vyber(fr, 0, '1'), 2,'9');
         fr.close();
         String radka;
