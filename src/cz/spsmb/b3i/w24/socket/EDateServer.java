0cca7b46b52b96fa71f7eea34979acc95fdbca47
stemberk <stemberk@spsmb.cz>
2022-02-16 11:19:57 +0100

    Reverse server, client

--- src/cz/spsmb/b3i/w24/socket/EDateServer.java
+++ src/cz/spsmb/b3i/w24/socket/EDateServer.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.GregorianCalendar;
 
+//Vytvoření serveru, který vrací údaj o datumu a čase. Ověřit možné pomocí klienta ASocketTimeClient
 public class EDateServer {
     public static void main(String[] args) throws IOException {
         int port = 8081;
@@ -13,8 +16,7 @@ public class EDateServer {
             while (true) {
                 try (var socket = listener.accept()) {
                     try (var pw = new PrintWriter(socket.getOutputStream(), true)) {
-
-                        pw.println(LocalDate.now());
+                        pw.println(LocalDateTime.now());
                     }
                 }
             }
